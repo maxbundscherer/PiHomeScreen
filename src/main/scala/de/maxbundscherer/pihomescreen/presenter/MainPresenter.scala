a3f4deb74caadb57bda4eecc7383bb3d39e1da8d
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-03-20 19:19:24 +0100

    add fake slider

--- src/main/scala/de/maxbundscherer/pihomescreen/presenter/MainPresenter.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/presenter/MainPresenter.scala
@@ -1,15 +1,20 @@
 package de.maxbundscherer.pihomescreen.presenter
 
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, ChoiceBox, TextArea, TextField}
+import scalafx.scene.control.{Button, ChoiceBox, ProgressBar, Slider, TextArea, TextField}
+import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.GridPane
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class MainPresenter(private val btnTopLeft: Button) {
+class MainPresenter(private val btnTopLeft: Button, private val prbMiddle: ProgressBar) {
 
   def btnTopLeft_onAction(event: ActionEvent): Unit = {
     this.btnTopLeft.setText("Hello!")
+  }
+
+  def prbMiddle_onMouseMoved(event: MouseEvent): Unit = {
+    prbMiddle.progress = event.x / prbMiddle.width.value
   }
 
 }
