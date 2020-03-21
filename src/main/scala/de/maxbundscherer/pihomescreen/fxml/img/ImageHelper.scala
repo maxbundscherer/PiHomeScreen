2e47a4e6d0c0db21a26d65c111d5f4f159e2006f
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-03-21 13:46:18 +0100

    Add background

---
+++ src/main/scala/de/maxbundscherer/pihomescreen/fxml/img/ImageHelper.scala
@@ -0,0 +1,22 @@
+package de.maxbundscherer.pihomescreen.fxml.img
+
+import scalafx.scene.image.Image
+import scalafx.scene.layout.{Background, BackgroundImage}
+
+object ImageHelper {
+
+  /**
+   * Converts image to Background
+   * @return Background
+   */
+  def getBackground(): Background = {
+
+    val res = getClass.getResourceAsStream("background.png")
+
+    if(res == null) throw new RuntimeException("Background not found")
+
+    val img = new Image(res)
+    new Background(Array(new BackgroundImage(img, null, null, null, null)))
+  }
+
+}
--- src/main/scala/de/maxbundscherer/pihomescreen/presenter/MainPresenter.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/presenter/MainPresenter.scala
@@ -1,21 +1,20 @@
 package de.maxbundscherer.pihomescreen.presenter
 
+import de.maxbundscherer.pihomescreen.fxml.img.ImageHelper
 import de.maxbundscherer.pihomescreen.utils.ProgressBarSlider
 
 import scalafx.event.ActionEvent
 import scalafx.scene.control.Button
 import scalafx.scene.input.MouseEvent
+import scalafx.scene.layout.Pane
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class MainPresenter(private val btnTopLeft: Button) extends ProgressBarSlider {
+class MainPresenter(private val panBackground: Pane) extends ProgressBarSlider {
 
-  def btnTopLeft_onAction(event: ActionEvent): Unit = {
-    this.btnTopLeft.setText("Hello!")
-  }
-
-  def prbMiddle_onMouseMoved(event: MouseEvent): Unit = {
-    println(s"Got ${updateProgressBar(event)} %")
+  def prb_onMouseMoved(event: MouseEvent): Unit = {
+    updateProgressBar(event)
+    panBackground.setBackground(ImageHelper.getBackground())
   }
 
 }
