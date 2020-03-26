25798b10d71c1812d3e10e145b34df34faa126df
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-03-26 18:15:14 +0100

    Improve logger

--- src/main/scala/de/maxbundscherer/pihomescreen/utils/Logger.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/utils/Logger.scala
@@ -2,7 +2,16 @@ package de.maxbundscherer.pihomescreen.utils
 
 class Logger(name: String) {
 
-  private def echo(prefix: String, msg: String): Unit = println(s"[$prefix]\t" + this.name + ":\t'" + msg + "'")
+  import java.util.Date
+  import java.text.SimpleDateFormat
+  import java.util.Calendar
+
+  private def getCalendar = Calendar.getInstance()
+  private def getTime: Date = this.getCalendar.getTime
+
+  private val timeFormat = new SimpleDateFormat("dd-M-yyyy HH:mm:ss")
+
+  private def echo(prefix: String, msg: String): Unit = println(s"[$prefix - ${this.timeFormat.format(getTime)}]\t" + this.name + ":\t'" + msg + "'")
 
   def error(msg: String): Unit = this.echo("Error", msg)
   def warn(msg: String): Unit = this.echo("Warn", msg)
