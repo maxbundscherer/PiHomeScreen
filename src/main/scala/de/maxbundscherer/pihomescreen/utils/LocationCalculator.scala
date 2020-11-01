67428bdfd5221ab245f0b03bfbbcdbfc1604ce13
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-11-01 19:29:56 +0100

    fix location

--- src/main/scala/de/maxbundscherer/pihomescreen/utils/LocationCalculator.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/utils/LocationCalculator.scala
@@ -6,16 +6,20 @@ object LocationCalculator {
 
   def calculateDistanceInKilometer(myLocation: Location, otherLocation: Location): Double = {
 
-    val latDis = Math.toRadians(myLocation.lat - otherLocation.lat)
-    val lngDis = Math.toRadians(myLocation.lon - otherLocation.lon)
+    val lat1: Double = myLocation.lat
+    val lat2: Double = otherLocation.lat
+    val lon1: Double = myLocation.lon
+    val lon2: Double = otherLocation.lon
 
-    val sinLat = Math.sin(latDis / 2)
-    val sinLng = Math.sin(lngDis / 2)
-
-    val a = sinLat * sinLat + (Math.cos(Math.toRadians(myLocation.lat)) * Math.cos(Math.toRadians(otherLocation.lat)) * sinLng * sinLng)
-
-    2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
+    val R = 6371 // Radius of the earth
+    val latDistance = Math.toRadians(lat2 - lat1)
+    val lonDistance = Math.toRadians(lon2 - lon1)
+    val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2)
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    var distance = R * c * 1000 // convert to meters
+    val height = 0
+    distance = Math.pow(distance, 2) + Math.pow(height, 2)
+    Math.sqrt(distance) / 1000
   }
 
 }
