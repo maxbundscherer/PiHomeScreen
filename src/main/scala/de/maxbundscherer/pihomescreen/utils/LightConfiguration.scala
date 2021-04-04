4d9ee5ed2c7987872daf695dd745bae234c1dedd
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-04-04 22:15:21 +0200

    Improve scenes

--- src/main/scala/de/maxbundscherer/pihomescreen/utils/LightConfiguration.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/utils/LightConfiguration.scala
@@ -6,20 +6,21 @@ trait LightConfiguration {
 
     type Light = Int
 
-    val KitchenTop        : Light = 7
-    val KitchenTable      : Light = 8
-    val KitchenBottom     : Light = 2
+    val KitchenTop: Light    = 7
+    val KitchenTable: Light  = 8
+    val KitchenBottom: Light = 2
 
-    val LivingRoomLeft    : Light = 5
-    val LivingRoomTruss   : Light = 11
-    val LivingRoomRight   : Light = 6
-    val LivingRoomCouch   : Light = 4
-    val LivingRoomCloset  : Light = 10
+    val LivingRoomLeft: Light   = 5
+    val LivingRoomTruss: Light  = 11
+    val LivingRoomRight: Light  = 6
+    val LivingRoomCouch: Light  = 4
+    val LivingRoomCloset: Light = 10
 
-    val BedroomBack       : Light = 9
-    val BedroomFront      : Light = 1
+    val BedroomBack: Light  = 9
+    val BedroomFront: Light = 1
 
-    val ALL_LIGHTS: Vector[Light] = Vector(KitchenTop,
+    val ALL_LIGHTS: Vector[Light] = Vector(
+      KitchenTop,
       KitchenTable,
       KitchenBottom,
       LivingRoomLeft,
@@ -37,18 +38,19 @@ trait LightConfiguration {
 
     import Lights._
 
-    type Room     = Vector[Light]
-    type GroupId  = Int
+    type Room    = Vector[Light]
+    type GroupId = Int
 
-    val Kitchen             : Room = Vector(KitchenTop, KitchenTable, KitchenBottom)
-    val LivingRoom          : Room = Vector(LivingRoomLeft, LivingRoomTruss, LivingRoomRight, LivingRoomCouch, LivingRoomCloset)
-    val Bedroom             : Room = Vector(BedroomBack, BedroomFront)
+    val Kitchen: Room = Vector(KitchenTop, KitchenTable, KitchenBottom)
+    val LivingRoom: Room =
+      Vector(LivingRoomLeft, LivingRoomTruss, LivingRoomRight, LivingRoomCouch, LivingRoomCloset)
+    val Bedroom: Room = Vector(BedroomBack, BedroomFront)
 
-    val KitchenGroupId      : GroupId = 2
-    val LivingRoomGroupId   : GroupId = 1
-    val BedroomGroupId      : GroupId = 3
+    val KitchenGroupId: GroupId    = 2
+    val LivingRoomGroupId: GroupId = 1
+    val BedroomGroupId: GroupId    = 3
 
-    val ALL_ROOMS           : Vector[Room] = Vector(Kitchen, LivingRoom, Bedroom)
+    val ALL_ROOMS: Vector[Room] = Vector(Kitchen, LivingRoom, Bedroom)
 
   }
 
@@ -57,39 +59,41 @@ trait LightConfiguration {
     import Rooms._
 
     /**
-     * Tuple (hueGroupId, hueSceneName)
-     */
+      * Tuple (hueGroupId, hueSceneName)
+      */
     type Scene = (GroupId, String)
 
-    val KitchenRead       : Scene = (KitchenGroupId, "L4A2frS39HBYt3b")
-    val KitchenRelax      : Scene = (KitchenGroupId, "TSdxxGkxdTPpEYU")
+    val KitchenRead: Scene  = (KitchenGroupId, "L4A2frS39HBYt3b")
+    val KitchenRelax: Scene = (KitchenGroupId, "TSdxxGkxdTPpEYU")
 
-    val LivingRoomRead    : Scene = (LivingRoomGroupId, "fezQWB7oMIW0Viu")
-    val LivingRoomRelax   : Scene = (LivingRoomGroupId, "2sy15kwZAhWZsAS")
-    val LivingRoomDimmed  : Scene = (LivingRoomGroupId, "KU-raZZ4nh9BxNM")
-    val LivingRoomDarkRed : Scene = (LivingRoomGroupId, "bS5U8wdOcJIEZED")
-    val LivingRoomSleep   : Scene = (LivingRoomGroupId, "vNwLwB2XeTVTZfn")
+    val LivingRoomRead: Scene   = (LivingRoomGroupId, "fezQWB7oMIW0Viu")
+    val LivingRoomRelax: Scene  = (LivingRoomGroupId, "2sy15kwZAhWZsAS")
+    val LivingRoomDimmed: Scene = (LivingRoomGroupId, "KU-raZZ4nh9BxNM")
+    val LivingRoomSky: Scene    = (LivingRoomGroupId, "-TsjoV5iDYMxo33")
+    val LivingRoomSleep: Scene  = (LivingRoomGroupId, "vNwLwB2XeTVTZfn")
 
-    val BedroomRead       : Scene = (BedroomGroupId, "hzovBAwEPoiMO9P")
-    val BedroomNightLight : Scene = (BedroomGroupId, "pxX-dtLRGZ-lBTP")
-    val BedroomRelax      : Scene = (BedroomGroupId, "jy0fpjxdpEhzKHz")
-    val BedroomRed        : Scene = (BedroomGroupId, "0tVU-NjBn0BIZ4d")
-    val BedroomSleep      : Scene = (BedroomGroupId, "4yLVlXazhSV1xqw")
+    val BedroomRead: Scene       = (BedroomGroupId, "hzovBAwEPoiMO9P")
+    val BedroomNightLight: Scene = (BedroomGroupId, "pxX-dtLRGZ-lBTP")
+    val BedroomRelax: Scene      = (BedroomGroupId, "jy0fpjxdpEhzKHz")
+    val BedroomRed: Scene        = (BedroomGroupId, "0tVU-NjBn0BIZ4d")
+    val BedroomSleep: Scene      = (BedroomGroupId, "4yLVlXazhSV1xqw")
 
   }
 
   object Routines {
 
     /**
-     * Tuple (Vector of scenes that should be activated, Vector of rooms that should be turned off)
-     */
+      * Tuple (Vector of scenes that should be activated, Vector of rooms that should be turned off)
+      */
     type Routine = (Vector[Scenes.Scene], Vector[Rooms.Room])
 
-    val WakeUp  : Routine  = ( Vector(Scenes.LivingRoomRead, Scenes.KitchenRead), Vector(Rooms.Bedroom) )
-    val Relax   : Routine  = ( Vector(Scenes.LivingRoomRelax, Scenes.KitchenRelax), Vector(Rooms.Bedroom) )
-    val DarkRed : Routine  = ( Vector(Scenes.LivingRoomDarkRed), Vector(Rooms.Bedroom, Rooms.Kitchen) )
-    val Sleep   : Routine  = ( Vector(Scenes.LivingRoomSleep, Scenes.BedroomSleep), Vector(Rooms.Kitchen) )
-    val AllOff  : Routine  = ( Vector(), Rooms.ALL_ROOMS )
+    val WakeUp: Routine = (Vector(Scenes.LivingRoomRead, Scenes.KitchenRead), Vector(Rooms.Bedroom))
+    val Relax: Routine =
+      (Vector(Scenes.LivingRoomRelax, Scenes.KitchenRelax), Vector(Rooms.Bedroom))
+    val DarkRed: Routine = (Vector(Scenes.LivingRoomSky), Vector(Rooms.Bedroom, Rooms.Kitchen))
+    val Sleep: Routine =
+      (Vector(Scenes.LivingRoomSleep, Scenes.BedroomSleep), Vector(Rooms.Kitchen))
+    val AllOff: Routine = (Vector(), Rooms.ALL_ROOMS)
 
   }
 
