a309e2cbe98e99ff6eae17d8d073ba29964c0da2
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-04-05 17:08:32 +0200

    improve calender

--- src/main/scala/de/maxbundscherer/pihomescreen/services/SimpleCalendarService.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/services/SimpleCalendarService.scala
@@ -3,26 +3,27 @@ package de.maxbundscherer.pihomescreen.services
 import de.maxbundscherer.pihomescreen.services.abstracts.CalendarService
 
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import java.util.{ Calendar, Date }
 
 class SimpleCalendarService extends CalendarService {
 
-  private def getCalendar = Calendar.getInstance()
+  private def getCalendar   = Calendar.getInstance()
   private def getTime: Date = this.getCalendar.getTime
 
+  private val hourFormat          = new SimpleDateFormat("HH")
   private val hourAndMinuteFormat = new SimpleDateFormat("HH:mm")
   private val dateFormat          = new SimpleDateFormat("dd.MM.")
 
   /**
-   * Get Hour and Minute
-   * @return e.g. 12:31
-   */
+    * Get Hour and Minute
+    * @return e.g. 12:31
+    */
   override def getHourAndMinuteToString: String = this.hourAndMinuteFormat.format(this.getTime)
 
   /**
-   * Get Date
-   * @return e.g. Mo, 01.02.
-   */
+    * Get Date
+    * @return e.g. Mo, 01.02.
+    */
   override def getDateToString: String = {
 
     val dayOfTheWeek: Int = this.getCalendar.get(Calendar.DAY_OF_WEEK)
@@ -40,5 +41,8 @@ class SimpleCalendarService extends CalendarService {
 
     dayString + ", " + this.dateFormat.format(this.getTime)
   }
+
+  override def getCurrentHour24: Int =
+    this.hourFormat.format(this.getTime).toInt
 
 }
