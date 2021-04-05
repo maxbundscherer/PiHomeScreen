51768a93b68efce728a7f6f80221e887770e0e08
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-04-05 17:08:59 +0200

    improve

--- src/main/scala/de/maxbundscherer/pihomescreen/services/abstracts/CalendarService.scala
+++ src/main/scala/de/maxbundscherer/pihomescreen/services/abstracts/CalendarService.scala
@@ -3,15 +3,17 @@ package de.maxbundscherer.pihomescreen.services.abstracts
 abstract class CalendarService {
 
   /**
-   * Get Hour and Minute
-   * @return e.g. 12:31
-   */
+    * Get Hour and Minute
+    * @return e.g. 12:31
+    */
   def getHourAndMinuteToString: String
 
   /**
-   * Get Date
-   * @return e.g. Mo, 01.02.
-   */
+    * Get Date
+    * @return e.g. Mo, 01.02.
+    */
   def getDateToString: String
+
+  def getCurrentHour24: Int
 
 }
